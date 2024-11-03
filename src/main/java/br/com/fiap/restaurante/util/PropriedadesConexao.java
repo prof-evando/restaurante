10bde6d78c877f973e6c57fa77e5244c9c412338
prof-evando <profevando.borges@fiap.com.br>
2024-11-03 00:06:02 -0300

    wip

--- src/main/java/br/com/fiap/restaurante/util/PropriedadesConexao.java
+++ src/main/java/br/com/fiap/restaurante/util/PropriedadesConexao.java
@@ -6,8 +6,8 @@ public class PropriedadesConexao {
 	
 	public static Properties obterPropriedades() {
 		Properties props = new Properties();
-		props.put("user", "pf2012");
-		props.put("password", "fiap24");
+		props.put("user", "usuariooo");
+		props.put("password", "senhaaaa");
 		return props;
 	}
 
